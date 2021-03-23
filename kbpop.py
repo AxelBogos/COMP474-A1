e395da9878bdb472944ebb09c2c0bd8bddecc724
Welton Jones <jones.welton.4@gmail.com>
2021-03-22 21:15:41 -0400

    able to add courses and some deets automatically

--- kbpop.py
+++ kbpop.py
@@ -3,22 +3,35 @@ from rdflib import Graph, URIRef, BNode, Literal, Namespace
 from rdflib.namespace import RDF,RDFS,XSD,FOAF,OWL
 
 DBR = Namespace("http://dbpedia.org/resource/")
-
-
+SCH = Namespace("http://a1.io/schema#")
+DAT = Namespace("http://a1.io/data#")
 
 g = Graph()
-g.parse('COMP474-A1/A1.ttl', format='turtle')
+g.parse('A1.ttl', format='turtle')
 
-for s,p,o in g:
-    print(s,p,o)
-
-
+#Independent triples
+g.add((DAT.Concordia, RDF.type, SCH.University))
 
 with open('/Users/welton/remoteproj/copy.csv','r') as data:
     r = csv.DictReader(data)
     for row in r:
         if row['Course code'] == 'COMP' and row['Course number'] in ['474','353']:
-            print(row)
+            #Create the course
+            cn = URIRef("http://a1.io/data#"+row['Course code']+row['Course number'])
+            g.add((cn, RDF.type, SCH.Course))
+            g.add((DAT.Concordia, SCH.Offers, cn))
+
+            #Add course deets
+            # g.add((cn, SCH.CourseName, Literal(row['Title'])))
+            g.add((cn, SCH.CourseSubject, Literal(row['Course code'])))
+            g.add((cn, SCH.CourseNumber, Literal(row['Course number'])))
+            # g.add((cn, SCH.CourseDescription, Literal(row['Description'])))
+            # g.add((cn, SCH.CourseWebsite, Literal(row['Website'])))
+
+            # g.add((cn, SCH.Outline, someURIToContent)))
+            
+
+        
 
 
-
+print(g.serialize(format="turtle").decode("utf-8"))
