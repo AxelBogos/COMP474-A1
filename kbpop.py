b6d21a74231377ffb290793a8be053ded8b1f0d6
AxelBogos <35436065+AxelBogos@users.noreply.github.com>
2021-03-24 21:38:07 -0400

    Added topics, outline and fixed bug

--- kbpop.py
+++ kbpop.py
@@ -1,21 +1,32 @@
 import csv
-from rdflib import Graph, URIRef, BNode, Literal, Namespace
-from rdflib.namespace import RDF,RDFS,XSD,FOAF,OWL
 import os
 from os import path
+
 import pandas as pd
+from rdflib import Graph, URIRef, Literal, Namespace
+from rdflib.namespace import RDF, RDFS
 
 DBR = Namespace("http://dbpedia.org/resource/")
+DBP = Namespace("http://dbpedia.org/property/")
 SCH = Namespace("http://a1.io/schema#")
 DAT = Namespace("http://a1.io/data#")
 TEACH = Namespace("http://linkedscience.org/teach/ns#")
 
 REGEN_CLEAN_CATALOG = False
 
-special_courses = [URIRef("http://a1.io/data#COMP353"),URIRef("http://a1.io/data#COMP474")]
+special_courses = [URIRef("http://a1.io/data#COMP353"), URIRef("http://a1.io/data#COMP474")]
+
+# All topics are dbpedia resources
+topics_353 = ['Database', 'Entity–relationship_model', 'Relational_database',
+              'Armstrong%27s_axioms', 'Armstrong%27s_axioms', 'Database_normalization',
+              'Relational_algebra', 'SQL', 'SQL', 'SQL', 'Datalog', 'Object_Definition_Language']
+
+topics_474 = ['Intelligent_system', 'Knowledge_graph', 'Ontology_(information_science)',
+              'SPARQL', 'Knowledge_base', 'Recommender_system', 'Machine_learning',
+              'Intelligent_agent', 'Text_mining']
+
 
 def populate_knowledge_base():
-
     g = Graph()
     g.parse('A1.ttl', format='turtle')
 
@@ -24,75 +35,67 @@ def populate_knowledge_base():
     #  so maybe we are not supposed to use the dbp entry as our key?
     g.add((DBR.Concordia_University, RDF.type, SCH.University))
 
-    with open('CLEAN_CATALOG.csv','r') as data:
+    with open('CLEAN_CATALOG.csv', 'r') as data:
         r = csv.DictReader(data)
         for row in r:
-            #Create the course
-            cn = URIRef("http://a1.io/data#"+row['Course code']+row['Course number'])
-            # TODO the CID in catalog doesnt seem to mean much intuitively. I think it should be cn everywhere
-            cid = URIRef("http://a1.io/data#"+row['Key'])
+            # Create the course
+            cn = URIRef("http://a1.io/data#" + row['Course code'] + row['Course number'])
             g.add((cn, RDF.type, SCH.Course))
 
             # Add to list of offered courses at Concordia
             g.add((DBR.Concordia_University, SCH.Offers, cn))
 
-            #Add course deets
+            # Add course details
             g.add((cn, TEACH.courseTitle, Literal(row['Title'])))
             g.add((cn, SCH.HasSubject, Literal(row['Course code'])))
             g.add((cn, SCH.CourseNumber, Literal(row['Course number'])))
             g.add((cn, TEACH.courseDescription, Literal(row['Description'])))
-            # TODO: Consider manually populating the description ^^ for these 2 courses. The dataset we have is gross
             g.add((cn, RDFS.seeAlso, Literal(row['Website'])))
 
             if cn in special_courses:
-                for i in range(1,12):
+                if row['Course number'] == '353':
+                    lecture_number = 13
+                else:
+                    lecture_number = 10
 
+                for i in range(1, lecture_number):
                     # Add Lectures
-                    lec_id = URIRef("http://a1.io/data#" + "{}{}Lec{}".format(row['Course code'],row['Course number'],i))
+                    lec_id = DAT["{}{}Lec{}".format(row['Course code'], row['Course number'], i)]
                     g.add((cn, SCH.HasLecture, lec_id))
-                    g.add((lec_id,RDF.type,SCH.Lecture))
+                    g.add((lec_id, RDF.type, SCH.Lecture))
                     g.add((lec_id, SCH.LectureNumber, Literal(i)))
 
+                    # Add topics
+                    if row['Course number'] == '353':
+                        g.add((lec_id, DBP.subject, DBR[topics_353[i - 1]]))
+                    else:
+                        g.add((lec_id, DBP.subject, DBR[topics_474[i-1]]))
+
                     # Add Labs
-                    lab_id = URIRef("http://a1.io/data#" + "{}{}Lab{}".format(row['Course code'], row['Course number'], i))
+                    lab_id = DAT["{}{}Lab{}".format(row['Course code'], row['Course number'], i)]
                     g.add((lab_id, RDF.type, SCH.Lab))
-                    g.add((lab_id,SCH.RelatedToLecture,lec_id))
+                    g.add((lab_id, SCH.RelatedToLecture, lec_id))
 
                     # Add Tutorials
-                    tut_id = URIRef("http://a1.io/data#" + "{}{}Tut{}".format(row['Course code'], row['Course number'], i))
+                    tut_id = DAT["{}{}Tut{}".format(row['Course code'], row['Course number'], i)]
                     g.add((tut_id, RDF.type, SCH.Tutorial))
-                    g.add((tut_id,SCH.RelatedToLecture,lec_id))
+                    g.add((tut_id, SCH.RelatedToLecture, lec_id))
 
-                    # Add Topics TODO: will be manual i think?
+                # Add Outline
+                dir_name = "c{}content".format(row['Course number'])
+                outline_path = os.path.join(dir_name, 'Outline.pdf')
+                if(os.path.isfile(outline_path)):
+                    g.add((cn, SCH.Outline, DAT[outline_path]))
 
                 # Add content
-                dir_name = "c{}content".format(row['Course number'])
                 for sub_dir in ['Readings', 'Slides', 'Worksheet', 'Other']:
                     dir_path = os.path.join(dir_name, sub_dir)
                     if os.path.isdir(dir_path):
                         for idf, f in enumerate(sorted(os.listdir(dir_path))):
-                            f_uri = URIRef("http://a1.io/data#" + path.join(dir_path,f))
-                            lec_id = URIRef("http://a1.io/data#" + "{}{}Lec{}".format(row['Course code'], row['Course number'], idf+1))
-                            g.add((f_uri, RDF.type, SCH+sub_dir))
-                            g.add((lec_id, SCH.HasMaterial,f_uri))
-
-                # Add outline TODO
-
-
-
-# TODO: Manually add topics and materials to satisfy the competency questions
-# TODO/ASK: What does "give each item an automatically generated URI" mean?
-# TODO: Add all the materials. literally all of it
-# c474 course topics
-    # Introduction to Intel Systems  (slides01)
-    # Knowledge Graphs               (slides02,worksheet01)
-    # Vocabularies & Ontologies      (slides03,worksheet02)
-# c353 course topics
-    # Introduction to Databases      (DB1)
-    # Basic SQL                      (DB1)
-    # ER Diagrams and Models         (DB2)
-    # Relation Data Model            (DB3)
-
+                            f_uri = DAT[path.join(dir_path, f)]
+                            lec_id = DAT["{}{}Lec{}".format(row['Course code'], row['Course number'], idf + 1)]
+                            g.add((f_uri, RDF.type, SCH[sub_dir]))
+                            g.add((lec_id, SCH.HasMaterial, f_uri))
     g.serialize(destination="out/kb_test.ttl", format="turtle")
 
 
@@ -104,8 +107,9 @@ def regenerate_catalog():
     """
     catalog = pd.read_csv('CATALOG.csv')
 
-    catalog = catalog.dropna(subset=['Course code', 'Course number']) #remove empty course codes and course names
+    catalog = catalog.dropna(subset=['Course code', 'Course number'])  # remove empty course codes and course names
     catalog.to_csv('CLEAN_CATALOG.csv', index=False)
+
 
 if __name__ == '__main__':
     if REGEN_CLEAN_CATALOG:
