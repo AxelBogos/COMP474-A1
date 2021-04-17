85cea1e62defabb937c00a84e479b92f7d78fa75
Xavier Morin <xavier10@live.ca>
2021-04-17 17:02:46 -0400

    small helper functions

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -7,8 +7,30 @@ from helper_functions import query_fuseki, load_query
 
 query2='prefix a1_data: <http://a1.io/data#> prefix dbp: <http://dbpedia.org/property/> prefix a1_schema: <http://a1.io/schema#> SELECT ?topic WHERE{a1_data:%s a1_schema:HasLecture ?lecture .?lecture a1_schema:LectureNumber 2 .?lecture dbp:subject ?topic}'
 
+# Question 1
+class Action_topic_of_lecture(Action):
+
+    def name(self) -> Text:
+        return "action_topic_of_lecture"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+         
+        Query=load_query("q1.txt")
+        Query=Query % (tracker.slots['course'],tracker.slots['lecture_number'])
+        Answer=query_fuseki(Query)
+
+        if(Answer):
+            message=f"Lecture {tracker.slots['lecture_number']} of {tracker.slots['course']} is about {Answer[0]}"
+        else:
+            message=f"I don't find any information about {tracker.slots['course']}."
+
+        dispatcher.utter_message(text=message)
+        return []
 
 
+# Question 2
 class Action_Course_Info(Action):
 
     def name(self) -> Text:
