c4c4f83f9d6842adb0aef82a79af2589b0a7dc62
Xavier Morin <xavier10@live.ca>
2021-04-17 21:35:17 -0400

    Q3

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -2,7 +2,8 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from helper_functions import SELECT_fuseki, ASK_fuseki,load_query, find_label
+from helper_functions import SELECT_fuseki, ASK_fuseki, load_query, find_label, generate_dbpedia_entities
+
 
 # Question 1
 class Action_topic_of_lecture(Action):
@@ -50,6 +51,33 @@ class Action_Course_Info(Action):
 
         dispatcher.utter_message(text=message)
         return []
+
+# Question 3
+class Action_Which_course_teaches_about(Action):
+
+    def name(self) -> Text:
+        return "action_which_course_in_university_teaches_about_subject"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+         
+        
+        subject=tracker.slots['topic']
+        subject=generate_dbpedia_entities(subject)
+
+        Query=load_query("q3.txt")
+        Query=Query %(tracker.slots['university'], subject)
+        Answer=SELECT_fuseki(Query)
+
+        if(Answer):
+            message=f"{Answer[0]} teaches {subject}."
+        else:
+            message=f"I don't find any course in {tracker.slots['university']} about {subject}."
+
+        dispatcher.utter_message(text=message)
+        return []
+
 # Question 5
 class Action_Course_Link(Action):
 
